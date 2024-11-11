2ef5f614d526595abb421586dfd7699910b1aee4
marwanmouradd <marwan.mourad.94@gmail.com>
2024-11-11 19:10:58 +0200

    Save work in progress

--- gradproj/lib/main.dart
+++ gradproj/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'Screens/SignupPage.dart';
-import 'Screens/LoginPage.dart';
+import 'screens/SignupPage.dart';
+import 'screens/LoginPage.dart';
 import 'package:http/http.dart' as http;
 import'dart:convert';
 
---
+++ gradproj/lib/models/property.dart
@@ -0,0 +1,17 @@
+class Property {
+  final String title;
+  final double price;
+  final String description;
+  final int bedrooms;
+  final int bathrooms;
+  final double area;
+
+  Property({
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.bedrooms,
+    required this.bathrooms,
+    required this.area,
+  });
+}
